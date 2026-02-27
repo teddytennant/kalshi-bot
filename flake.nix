aff0352eb698d20f84fc2217f50cfcba1c7ac871
Teddy Tennant <teddytennant@icloud.com>
2026-02-26 20:12:36 -0500

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "kalshi-bot - paper trading bot for Kalshi prediction markets";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        python = pkgs.python313;
+        pythonPkgs = python.pkgs;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            (python.withPackages (ps: with ps; [
+              requests
+              pytest
+              pytest-cov
+            ]))
+          ];
+
+          shellHook = ''
+            echo "kalshi-bot dev shell ready"
+            echo "Python: $(python --version)"
+            export PYTHONPATH="$PWD/src:$PYTHONPATH"
+          '';
+        };
+      }
+    );
+}
